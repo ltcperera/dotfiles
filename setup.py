1687388eb1ea610a26e0dce3db95f479b93089d6
Leander Perera <6547436+ltcperera@users.noreply.github.com>
2023-01-29 20:13:09 -0800

    Cleanup code for Linux distro detection.

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ OS_NAME_LINUX = "Linux"
 OS_NAME_UNKNOWN = "Unknown"
 
 def get_host_os():
+    """Returns the OS on which this script is running."""
     os_name = platform.system()
     if os_name == "Windows":
         return OS_NAME_WINDOWS
@@ -28,7 +29,12 @@ def get_host_os():
     else:
         return OS_NAME_UNKNOWN
 
-def get_linux_flavor():
+def get_linux_distro():
+    """For Linux platforms, detects between Ubuntu, Arch and Manjaro distributions"""
+
+    # Note: We could use the new distro package, but thats not part of the 
+    #   standard python packages yet. Using this following method for now
+    #   so that we can reduce the distro package as a dependency.
     uname_output = subprocess.run(['uname', '-a'], stdout=subprocess.PIPE)
     os_info = uname_output.stdout.decode().strip().lower()
     if "ubuntu" in os_info:
@@ -42,9 +48,9 @@ detected_os = get_host_os()
 print("Detected OS:", detected_os)
 
 if detected_os == OS_NAME_MAC:
-    print("Bingo")
+    print("Execute MacOS setup script...")
 
 if detected_os == OS_NAME_LINUX:
-    name = get_linux_flavor();
-    print("OS:", name)
+    distro = get_linux_distro();
+    print("Linux distribution:", distro)
 
