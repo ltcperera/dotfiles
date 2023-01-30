bc8d605142589f09955de319844d8d5b412ef516
Leander Perera <6547436+ltcperera@users.noreply.github.com>
2023-01-29 22:42:34 -0800

    Add MacOS specific dependency installation

--- scripts/setup-macos.sh
+++ scripts/setup-macos.sh
@@ -1,1 +1,62 @@
-echo "Mac OS Setup"
+#
+# Setup for MacOS platforms
+#
+
+echo "Mac OS Setup..."
+source scripts/posix-functions.sh
+
+# Install homebrew if not found
+if is_executable_not_found brew; then
+    echo "Homebrew not found..."
+    if is_user_response_yes; then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+    echo
+fi
+
+# Install Tmux if not found
+if is_executable_not_found tmux; then
+    echo "Tmux not found..."
+    if is_user_response_yes; then
+        brew install tmux
+    fi
+    echo
+fi
+
+# Install Neovim if not found
+if is_executable_not_found nvim; then
+    echo "Neovim not found..."
+    if is_user_response_yes; then
+        brew install nvim 
+    fi
+    echo
+fi
+
+
+# Install Exa (https://github.com/ogham/exa)
+if is_executable_not_found exa; then
+    echo "Cannot find exa utility..."
+    if is_user_response_yes; then
+        brew install exa
+    fi
+    echo
+fi
+
+# Install Bat (https://github.com/sharkdp/bat)
+if is_executable_not_found bat; then
+    echo "Cannot find bat utility..."
+    if is_user_response_yes; then
+        brew install bat
+    fi
+    echo
+fi
+
+# Install fzf (https://github.com/junegunn/fzf)
+if is_executable_not_found fzf; then
+    echo "Cannot find fzf utility..."
+    if is_user_response_yes; then
+        brew install fzf
+    fi
+    echo
+fi
+
