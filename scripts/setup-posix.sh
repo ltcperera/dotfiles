f33d7c424b9f1c14e499135791a5a100695d68af
Leander Perera <6547436+ltcperera@users.noreply.github.com>
2023-01-31 17:52:46 -0800

    Add Oh My Zsh setup

--- scripts/setup-posix.sh
+++ scripts/setup-posix.sh
@@ -11,10 +11,16 @@ if [ "$SHELL" != "/bin/zsh" ]; then
     exit 1
 fi
 
-# Install oh-my-zsh if not installed
+# Install oh-my-zsh themes and plugins if not installed
 if [ ! -d ~/.oh-my-zsh ]; then
     echo "Oh My Zsh not found..."
     if is_user_response_yes; then
+        # Install oh-my-zsh
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        # Install Powerlevel10k theme
+        git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+        # Install autosuggestions and syntax highlighting plugins
+        git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     fi
 fi
