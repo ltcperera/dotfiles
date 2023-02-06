89b138d3b73bc774747849fbfeca04b05f7676fc
Leander Perera <6547436+ltcperera@users.noreply.github.com>
2023-02-05 17:09:16 -0800

    Fix syntax error with Ubuntu/Debian

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ if [ "$os_name" == "Darwin" ]; then
 else
     if [ -f "/etc/os-release" ]; then
         source /etc/os-release
-        if [ "$ID" == "debian" || "$ID" == "ubuntu" ]; then
+        if [ "$ID" == "debian" ] || [ "$ID" == "ubuntu" ]; then
             source scripts/setup-ubuntu.sh
         fi
     fi
