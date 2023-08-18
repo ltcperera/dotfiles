c456d18c92bf0d56ad712b3022317fb7e7681cc2
Leander Perera <6547436+ltcperera@users.noreply.github.com>
2023-08-18 16:27:31 -0700

    Update treesitter configuration

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,6 +5,43 @@ return {
         dependencies = {
             'nvim-treesitter/nvim-treesitter-textobjects',
         },
+        config = function() 
+            local treesitter = require("nvim-treesitter.configs")
+
+            treesitter.setup({
+                -- enable syntax highlighting
+                highlight = {
+                    enable = true,
+                },
+                -- enable autotagging (w/ nvim-ts-autotag plugin)
+                autotag = { enable = true },
+                -- ensure these language parsers are installed
+                ensure_installed = {
+                    "json",
+                    "javascript",
+                    "typescript",
+                    "tsx",
+                    "yaml",
+                    "html",
+                    "css",
+                    "markdown",
+                    "svelte",
+                    "graphql",
+                    "bash",
+                    "lua",
+                    "vim",
+                    "dockerfile",
+                    "gitignore",
+                    "c",
+                    "cpp",
+                    "go",
+                    "rust",
+                    "python",
+                },
+                -- auto install above language parsers
+                auto_install = true,
+            })
+        end,
         build = ':TSUpdate',
     },
 }
