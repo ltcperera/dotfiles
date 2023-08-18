426c5594018cb47309720a2dc29eb168b8931649
Leander Perera <6547436+ltcperera@users.noreply.github.com>
2023-08-18 14:10:30 -0700

    Customized lualine theme colors

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,13 +3,35 @@ return {
     {
         'nvim-lualine/lualine.nvim',
         -- See `:help lualine.txt`
-        opts = {
-            options = {
-                icons_enabled = false,
-                theme = 'auto',
-                component_separators = '|',
-                section_separators = '',
-            },
-        },
+        config = function()
+            local lualine = require("lualine")
+            local lualine_nightfly = require("lualine.themes.nightfly")
+
+            -- Customize theme colors
+            local new_colors = {
+                blue = "#65D1FF",
+                green = "#3EFFDC",
+                violet = "#FF61EF",
+                yellow = "#FFDA7B",
+                black = "#000000",
+            }
+
+            lualine_nightfly.normal.a.bg = new_colors.blue
+            lualine_nightfly.insert.a.bg = new_colors.green
+            lualine_nightfly.visual.a.bg = new_colors.violet
+            lualine_nightfly.command = {
+                a = {
+                    gui = "bold",
+                    bg = new_colors.yellow,
+                    fg = new_colors.black,
+                },
+            }
+
+            lualine.setup({
+                options = {
+                    theme = lualine_nightfly
+                }
+            })
+        end
     },
 }
