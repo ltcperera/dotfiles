64828e5d22404c502edf56c0c9137c0c1f23fd66
Leander Perera <6547436+ltcperera@users.noreply.github.com>
2023-08-27 15:45:12 -0700

    Disable lspsaga winbar

--- .config/nvim/lua/plugins/lsp/lspsaga.lua
+++ .config/nvim/lua/plugins/lsp/lspsaga.lua
@@ -1,7 +1,17 @@
 return {
     'nvimdev/lspsaga.nvim',
     config = function()
-        require('lspsaga').setup({})
+        require('lspsaga').setup({
+            -- Keybinds for navigation in lspsaga window
+            scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
+            -- Use enter to open file with definition preview
+            definition = {
+                edit = "<CR>",
+            },
+            symbol_in_winbar = {
+                enable = false,
+            }
+        })
     end,
     dependencies = {
         'nvim-treesitter/nvim-treesitter', -- optional
