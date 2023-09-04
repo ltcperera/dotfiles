83ad3be67b5ce9f3f81689ed8b3e592894696b2e
Leander Perera <6547436+ltcperera@users.noreply.github.com>
2023-09-04 08:59:16 -0700

    Cleanup keymaps file

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -15,8 +15,6 @@ keymap.set('n', '<F3>', ":nohl<CR>") -- Remove highlight
 keymap.set('n', '<leader>\\', '<C-w>v', { desc = 'Split window vertically' })
 keymap.set('n', '<leader>-', '<C-w>s', { desc = 'Split window horizontally' })
 keymap.set('n', '<leader>=', '<C-w>=', { desc = 'Make split windows equal width' })
--- Temporarily commenting out
--- keymap.set("n", "<leader>w", ":Bwipeout<CR>") -- close current split window
 
 -- Ctrl-s for saving
 keymap.set('n', '<C-s>', ':w<CR>', { desc = 'Save current file' })
