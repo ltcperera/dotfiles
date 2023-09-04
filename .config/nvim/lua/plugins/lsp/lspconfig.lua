e4449968caad49452209793346c3d9c63caa7e42
Leander Perera <6547436+ltcperera@users.noreply.github.com>
2023-09-04 16:55:15 -0700

    Add descriptions to some commands

--- .config/nvim/lua/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -17,23 +17,22 @@ return {
         },
         config = function()
             local cmp_nvim_lsp = require("cmp_nvim_lsp")
-            local typescript = require("typescript")
             local keymap = vim.keymap
 
             -- Setup neovim lua configuration
             require('neodev').setup()
 
             local on_attach = function(client, bufnr)
- 
+
                 -- keybind options
                 local opts = { noremap = true, silent = true, buffer = bufnr }
 
                 -- set keybinds
-                keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
-                keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
-                keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
-                keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
-                keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
+                keymap.set("n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", { desc = "Show references" }) -- show definition, references
+                keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", { desc = "Go to declaration" }) -- got to declaration
+                keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", { desc = "Peek/edit definition" }) -- see definition and make edits in window
+                keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { desc = "Go to implementation" }) -- go to implementation
+                keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", { desc = "See available code actions" }) -- see available code actions
                 keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
                 keymap.set("n", "<leader>d", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
                 keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
@@ -53,7 +52,7 @@ return {
             -- used to enable autocompletion (assign to every lsp server config)
             local capabilities = cmp_nvim_lsp.default_capabilities()
             capabilities.offsetEncoding = "utf-8"
-            
+
             -- Specify servers to be configured
             local servers = {
                 clangd = {},
