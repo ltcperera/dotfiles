169ce9d7a9ffa5bd2808745c7fc2e41c2f2daaba
Leander Perera <6547436+ltcperera@users.noreply.github.com>
2023-09-04 22:43:12 -0700

    Remove trailing space

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
     -- Load our theme, nightfly
-    { 
-        "bluz71/vim-nightfly-colors", 
-        name = "nightfly", 
-        lazy = false, 
+    {
+        "bluz71/vim-nightfly-colors",
+        name = "nightfly",
+        lazy = false,
         priority = 1000,
         config = function()
             vim.cmd.colorscheme 'nightfly'
