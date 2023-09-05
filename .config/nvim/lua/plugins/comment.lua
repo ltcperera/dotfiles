0e12dee476db2418a1f514aabb057fefdf2dac2e
Leander Perera <6547436+ltcperera@users.noreply.github.com>
2023-09-04 23:37:53 -0700

    Added Ctrl+/ to comment a line

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,4 +1,11 @@
 return {
     -- "gc" to comment visual regions/lines
-    { 'numToStr/Comment.nvim', opts = {} },
+    {
+        'numToStr/Comment.nvim', opts = {},
+         config = function()
+            require('Comment').setup()
+            local api = require('Comment.api')
+            vim.keymap.set('n', "<C-/>", api.toggle.linewise.current, { desc = 'Comment line with Ctrl + /' }) -- comment line with Ctrl + /
+         end
+    }
 }
